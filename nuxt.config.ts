16b7fd40f4e912b382cb43b58ae48f57616e4620
danielcrubens <dcr.tecnologi@gmail.com>
2024-11-24 12:08:59 -0300

    9

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  router: { base: '/pages/' },
+ /*  router: { base: '/pages/' }, */
   app: {
- /*    baseURL: '', */
+    baseURL: '/pages/',
    /*  buildAssetsDir: 'assets' */
   },
  /*  image: {
