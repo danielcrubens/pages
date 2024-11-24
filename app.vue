cee87138183755a7a530a861da1905ae2db3f79c
danielcrubens <dcr.tecnologi@gmail.com>
2024-11-24 09:54:00 -0300

    6

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>foi7</h1>
-    <img src="/public/public.svg" alt="foi">
+    <img src="@/public/public.svg" alt="foi">
+    <img src="@/assets/images/teste2.svg" alt="teste">
   </div>
 </template>
