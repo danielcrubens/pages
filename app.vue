0d3aa6a0f749f04d578d5fd6b42fae3880563ed8
danielcrubens <dcr.tecnologi@gmail.com>
2024-11-24 12:30:27 -0300

    9

--- app.vue
+++ app.vue
@@ -2,9 +2,11 @@
   <div>
     <h1>dev</h1>
 <!-- <img :src="publicImage" alt="foiiiiii"> --> 
-    <img src="~/public/images/public.svg" alt="dan1">
+    <img src="/public.svg" alt="dan1">
     <img src="~/assets/images/teste2.svg" alt="dan2">
     <img :src="assetImage" alt="dan3">
+    <img :src="assetImage2" alt="dan32">
+
   </div>
 </template>
 
@@ -13,4 +15,6 @@
 
 // Opção 2
 import assetImage from '@/public/images/public.svg'
+import assetImage from '@/public/public.svg'
+
 </script>
