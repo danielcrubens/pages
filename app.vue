a3cfa7c372cadcadc226d5a249d9e3c0aaac07e0
danielcrubens <dcr.tecnologi@gmail.com>
2024-11-25 09:36:48 -0300

    9

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <h1>dev</h1>
 <!-- <img :src="publicImage" alt="foiiiiii"> --> 
 <img src="/v2osk-1Z2niiBPg5A-unsplash.jpg" loading="lazy">
-<img src="/pages/qingbao-meng-01_igFr7hd4-unsplash.jpg" loading="lazy">
+<img src="/qingbao-meng-01_igFr7hd4-unsplash.jpg" loading="lazy">
 
    <!--  <img :src="assetImage" alt="dan3">
     <img :src="assetImage2" alt="dan32"> -->
